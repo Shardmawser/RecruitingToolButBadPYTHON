95da697c7ddbdcb1ec88fecbc0c6cc2bb41ea4e1
Shardmawser <abantolin777@gmail.com>
2021-06-04 19:31:51 +0800

    Fixed retarded grammar

--- hotkey.py
+++ hotkey.py
@@ -9,15 +9,15 @@ def on_press(key):
         k = str(key)
         
         if k == 'Key.f1':
-                pag.write('Is it ok if I ask you a few questions first?')
+                pag.write('Is it ok if I ask you a question first?')
         if k == 'Key.f2':
-                pag.write("May I know what your favorite anime is?")
+                pag.write("What is your favorite anime?")
         if k == 'Key.f3':
                 pag.write('Ok, please follow this link to our discord server: discord.gg/hentai.')
         if k == 'Key.f4':
                 pag.write("Once you're there, hit the blue heart in the clan entrance and answer the bot's questions")
         if k == 'Key.f5':
-                pag.write("Under 'Who is the Recruiter that you spoke to?' write 'AverageCheeseEnjoyer' and under 'Which clan do you plan to be joining?' type in 'Waifu'")
+                pag.write("Under 'Who is the Recruiter that you spoke to?' write 'Shardmawser' and under 'Which clan do you plan to be joining?' type in 'Waifu'")
         if k == 'Key.f6':
                 pag.write("Feel free to message me once you've finished your application or you encounter any problems.")
         
