deca408091db309472090247a24d8734cdc7d93a
Shardmawser <abantolin777@gmail.com>
2021-06-04 20:36:11 +0800

    Changed window title to match functionality

--- loop.py
+++ loop.py
@@ -8,7 +8,7 @@ from ctypes import wintypes
 from pynput.mouse import Button, Controller
 
 
-ctypes.windll.kernel32.SetConsoleTitle("Loop")
+ctypes.windll.kernel32.SetConsoleTitleW("Loop")
 user32 = ctypes.WinDLL('user32', use_last_error=True)
 
 INPUT_MOUSE    = 0
