697628699a50fc80dc30d2756cdee83eea0fc775
Shardmawser <abantolin777@gmail.com>
2021-06-04 18:57:59 +0800

    first commit

---
+++ hotkey.py
@@ -0,0 +1,34 @@
+import pynput
+from pynput import mouse
+from pynput.keyboard import Key, Listener
+import pyautogui
+
+pag = pyautogui
+
+def on_press(key):
+        k = str(key)
+        
+        if k == 'Key.f1':
+                pag.write('Is it ok if I ask you a few questions first?')
+        if k == 'Key.f2':
+                pag.write("May I know what your favorite anime is?")
+        if k == 'Key.f3':
+                pag.write('Ok, please follow this link to our discord server: discord.gg/hentai.')
+        if k == 'Key.f4':
+                pag.write("Once you're there, hit the blue heart in the clan entrance and answer the bot's questions")
+        if k == 'Key.f5':
+                pag.write("Under 'Who is the Recruiter that you spoke to?' write 'AverageCheeseEnjoyer' and under 'Which clan do you plan to be joining?' type in 'Waifu'")
+        if k == 'Key.f6':
+                pag.write("Feel free to message me once you've finished your application or you encounter any problems.")
+        
+def on_release(key):
+        if key == Key.esc:
+                return
+
+with Listener(on_press = on_press, on_release = on_release) as listener:
+        listener.join()
+
+
+
+            
+
---
+++ loop.py
@@ -0,0 +1,128 @@
+import pynput
+from pynput import mouse
+from pynput.keyboard import Key, Listener
+import pyautogui
+import time
+import ctypes
+from ctypes import wintypes
+from pynput.mouse import Button, Controller
+
+user32 = ctypes.WinDLL('user32', use_last_error=True)
+
+INPUT_MOUSE    = 0
+INPUT_KEYBOARD = 1
+INPUT_HARDWARE = 2
+
+KEYEVENTF_EXTENDEDKEY = 0x0001
+KEYEVENTF_KEYUP       = 0x0002
+KEYEVENTF_UNICODE     = 0x0004
+KEYEVENTF_SCANCODE    = 0x0008
+
+MAPVK_VK_TO_VSC = 0
+
+# msdn.microsoft.com/en-us/library/dd375731
+VK_TAB  = 0x09
+VK_MENU = 0x12
+VK_RETURN = 0x0D
+
+# C struct definitions
+
+wintypes.ULONG_PTR = wintypes.WPARAM
+
+class MOUSEINPUT(ctypes.Structure):
+    _fields_ = (("dx",          wintypes.LONG),
+                ("dy",          wintypes.LONG),
+                ("mouseData",   wintypes.DWORD),
+                ("dwFlags",     wintypes.DWORD),
+                ("time",        wintypes.DWORD),
+                ("dwExtraInfo", wintypes.ULONG_PTR))
+
+class KEYBDINPUT(ctypes.Structure):
+    _fields_ = (("wVk",         wintypes.WORD),
+                ("wScan",       wintypes.WORD),
+                ("dwFlags",     wintypes.DWORD),
+                ("time",        wintypes.DWORD),
+                ("dwExtraInfo", wintypes.ULONG_PTR))
+
+    def __init__(self, *args, **kwds):
+        super(KEYBDINPUT, self).__init__(*args, **kwds)
+        # some programs use the scan code even if KEYEVENTF_SCANCODE
+        # isn't set in dwFflags, so attempt to map the correct code.
+        if not self.dwFlags & KEYEVENTF_UNICODE:
+            self.wScan = user32.MapVirtualKeyExW(self.wVk,
+                                                 MAPVK_VK_TO_VSC, 0)
+
+class HARDWAREINPUT(ctypes.Structure):
+    _fields_ = (("uMsg",    wintypes.DWORD),
+                ("wParamL", wintypes.WORD),
+                ("wParamH", wintypes.WORD))
+
+class INPUT(ctypes.Structure):
+    class _INPUT(ctypes.Union):
+        _fields_ = (("ki", KEYBDINPUT),
+                    ("mi", MOUSEINPUT),
+                    ("hi", HARDWAREINPUT))
+    _anonymous_ = ("_input",)
+    _fields_ = (("type",   wintypes.DWORD),
+                ("_input", _INPUT))
+
+LPINPUT = ctypes.POINTER(INPUT)
+
+def _check_count(result, func, args):
+    if result == 0:
+        raise ctypes.WinError(ctypes.get_last_error())
+    return args
+
+user32.SendInput.errcheck = _check_count
+user32.SendInput.argtypes = (wintypes.UINT, # nInputs
+                             LPINPUT,       # pInputs
+                             ctypes.c_int)  # cbSize
+
+# Functions
+
+def PressKey(hexKeyCode):
+    x = INPUT(type=INPUT_KEYBOARD,
+              ki=KEYBDINPUT(wVk=hexKeyCode))
+    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    x = INPUT(type=INPUT_KEYBOARD,
+              ki=KEYBDINPUT(wVk=hexKeyCode,
+                            dwFlags=KEYEVENTF_KEYUP))
+    user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
+
+
+
+
+pag = pyautogui
+myMouse = Controller()
+
+count = 0
+while True:
+        
+        if count != 1:
+                time.sleep(5)
+                pag.write(':ducats:{Clan}:ducats: Largest active weeb community in Warframe! | Moon Clan | Research, Dojo, Events! | Join our amazing community! MR 0+')
+                time.sleep(2)
+                PressKey(VK_RETURN)
+                myMouse.position = (432, 1059)
+                myMouse.click(Button.left)
+                myMouse.position = (91, 1019)
+                myMouse.click(Button.left)
+                count += 1
+        time.sleep(120)
+        pag.write(':ducats:{Waifu Kingdom} is recruiting! (DM me to be recruited!) Rank 10 Moon Clan | Max Research | Dry Dock | Active Helpful Veterans | Giveaways | MR 0+ | REQUIREMENTS: Discord')
+        time.sleep(2)
+        PressKey(VK_RETURN)
+        myMouse.position = (432, 1059)
+        myMouse.click(Button.left)
+        myMouse.position = (91, 1019)
+        myMouse.click(Button.left)
+        
+
+
+
+
+
+            
+
